ccc7fce97ffb2db24d232a69117ab551d9ce2bce
Yifan Sun <sunyifan112358@gmail.com>
2017-02-01 10:11:21 -0500

    network in mem-config and standalong net-config

--- test2/run.sh
+++ test2/run.sh
@@ -2,7 +2,6 @@
   --x86-sim detailed --x86-config x86-config.ini \
   --si-sim detailed --si-config si-config.ini \
   --mem-config mem-config.ini \
-  --net-config net-config.ini \
   --trace trace.txt.bz2 \
     ~/multi2sim/m2s-bench-amdsdk-2.5/BlackScholes/BlackScholes \
     --load ~/multi2sim/m2s-bench-amdsdk-2.5/BlackScholes/BlackScholes_Kernels.bin \
